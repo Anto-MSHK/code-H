51218b8f063b14991772fc634edc9c041c51580f
Anto-MSHK <anjiilop25@gmail.com>
2022-10-21 23:56:55 +0300

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "antd";
+import React from "react";
+import "antd/dist/antd.css";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <Button className="button">...</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/state/state.ts
@@ -0,0 +1,16 @@
+import { combineReducers, legacy_createStore as createStore } from "redux";
+// import { scheduleReducer } from "./schedule/reducer";
+// import { ScheduleGroupsStateT } from "./schedule/types";
+
+// export interface RootState {
+//   schedule: ScheduleGroupsStateT;
+// }
+
+// const rootReducer = combineReducers<RootState>({
+//   schedule: scheduleReducer,
+// });
+
+// const store = createStore(rootReducer);
+
+// export type AppDispatch = typeof store.dispatch;
+// export default store;
