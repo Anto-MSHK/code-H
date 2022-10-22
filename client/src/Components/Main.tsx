4aa78b8a517f23d09e5a0901cba7a6d3c961ec7c
DULCETq <maks.lebedev0404@mail.ru>
2022-10-22 16:38:48 +0300

    FIXED

--- client/src/Components/Main.tsx
+++ client/src/Components/Main.tsx
@@ -1,23 +1,13 @@
 import { Content } from "antd/lib/layout/layout";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
-
-export const Main = () => {
-  return (
-    <Content style={{ padding: "0 24px", minHeight: 280 }}>
-      {/*<Routes>*/}
-      {/*  <Route path="/auth" element={<div></div>} />*/}
-      {/*  <Route path="/main/" element={<div></div>} />*/}
-      {/*  <Route path="/settings/" element={<div></div>} />*/}
-      {/*</Routes>*/}
-
-=======
 import { Board } from "./Board/Board";
-import "./Main.css"
+import "./Main.css";
 import {Settings} from "./Settings/Settings";
 
+
 export const Main = () => {
+  
   return (
     <Content style={{ padding: "0 24px", minHeight: 280 }} className={"main"}>
       <Routes>
@@ -25,7 +15,6 @@ export const Main = () => {
         <Route path="/main/" element={<Board title={"Все задачи"} />} />
         <Route path="/settings/" element={<Settings />} />
       </Routes>
->>>>>>> 1cc743a7fa6eded8abca0358283924805428ff96
     </Content>
   );
 };
