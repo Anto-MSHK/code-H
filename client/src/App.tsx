d1652a4a1772d13fd91975e54ac82ad03ab01070
TheKnightKing <zadirab@gmail.com>
2022-10-22 09:49:59 +0300

    Main and Menu error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,14 @@ import { Button } from "antd";
 import React from "react";
 import "antd/dist/antd.css";
 import "./App.css";
+import { Main } from "./Components/Main";
 function App() {
   return (
     <div className="App">
       <Button className="button">...</Button>
+      <Main/>
     </div>
+    
   );
 }
 
---
+++ client/src/Components/Main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Menu } from "antd";
+import MenuItems from "./MenuItems"
+import "./Main.css"
+
+export const Main =()=>{
+    return(
+        <div>
+            <MenuItems/>
+        </div>
+    );
+}
