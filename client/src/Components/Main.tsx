7c12c88efa2447ef428766af8fd15f7480a7cf1d
Rain <silverain1qw@gmail.com>
2022-10-22 12:57:47 +0300

    Cringe settings

--- client/src/Components/Main.tsx
+++ client/src/Components/Main.tsx
@@ -1,15 +1,17 @@
 import { Content } from "antd/lib/layout/layout";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import {Settings} from "./Settings/Settings";
 
 export const Main = () => {
   return (
     <Content style={{ padding: "0 24px", minHeight: 280 }}>
-      <Routes>
-        <Route path="/auth" element={<div></div>} />
-        <Route path="/main/" element={<div></div>} />
-        <Route path="/settings/" element={<div></div>} />
-      </Routes>
+      {/*<Routes>*/}
+      {/*  <Route path="/auth" element={<div></div>} />*/}
+      {/*  <Route path="/main/" element={<div></div>} />*/}
+      {/*  <Route path="/settings/" element={<div></div>} />*/}
+      {/*</Routes>*/}
+        <Settings />
     </Content>
   );
 };
